8cc363415ace48010fa901bdabb4374e3a570086
Youngrok Park <yrpark99@kaongroup.com>
2024-07-29 22:14:01 +0900

    vscode 설정 적용

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -1,3 +1,12 @@
 return {
-  { "Mofiqul/vscode.nvim" },
+  "Mofiqul/vscode.nvim",
+  config = function()
+    require('vscode').setup({
+      italic_comments = false,
+      transparent = true,
+      group_overrides = {
+        CursorLine = { bg = '#505050' },
+      }
+    })
+  end
 }
