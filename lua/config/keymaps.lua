853ab1bb82b33d3c33ffa894c225cec9b7cd0093
Youngrok Park <yrpark99@gmail.com>
2025-02-11 11:34:24 +0900

    키맵 정리

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,9 @@
 local Util = require("lazyvim.util")
 local map = Util.safe_keymap_set
 
+-- Exit(quit all)
+vim.keymap.set({ "n", "i", "v" }, "<A-x>", "<Esc><Cmd>quitall<CR>", { noremap = true, silent = true })
+
 -- Shift-Del
 vim.keymap.set({ "n", "v", "i" }, "<S-Del>", function()
   local mode = vim.api.nvim_get_mode().mode
@@ -26,8 +29,8 @@ vim.keymap.set({ "n", "v", "i" }, "<A-Del>", function()
 end, { expr = true, desc = "Delete from cursor to end of line" })
 
 -- LSP related
-map("n", "<C-l>", "<cmd>LspStop<cr>", { desc = "Stop LSP" })
-map("n", "<C-r>", "<cmd>LspStart<cr>", { desc = "start LSP" })
+map("n", "<A-l>", "<Cmd>LspStop<CR>", { desc = "Stop LSP" })
+map("n", "<A-r>", "<Cmd>LspStart<CR>", { desc = "start LSP" })
 vim.keymap.set({ "n", "i", "v" }, "<F12>", function()
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
   vim.lsp.buf.definition()
@@ -38,14 +41,14 @@ vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 
 -- Reopen latest file tab
-vim.keymap.set("n", "<C-r>", "<cmd>ReopenLatest<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>br", "<cmd>ReopenLatest<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-r>", "<Cmd>ReopenLatest<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>br", "<Cmd>ReopenLatest<CR>", { noremap = true, silent = true })
 
 -- File tabs
-vim.keymap.set({ "n", "i", "v" }, "<A-Right>", "<Esc><cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<A-Left>", "<Esc><cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>b>", "<cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>b<", "<cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<A-Right>", "<Esc><Cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<A-Left>", "<Esc><Cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b>", "<Cmd>BufferLineMoveNext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b<", "<Cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true })
 
 -- Copy current file path
 vim.keymap.set("n", "<leader>fp", function()
