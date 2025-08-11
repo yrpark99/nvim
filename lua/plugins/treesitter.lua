d7d4cbdd83a85948260c53277336f836f2b610ac
Youngrok Park <yrpark99@gmail.com>
2025-08-11 11:25:05 +0900

    주요 언어용 Treesitter 자동 설치 추가

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bitbake",
+      "blueprint",
+      "c",
+      "cpp",
+      "cmake",
+      "css",
+      "go",
+      "java",
+      "javadoc",
+      "javascript",
+      "kotlin",
+      "lua",
+      "make",
+      "python",
+      "rust",
+      "typescript",
+      "zig"
+    },
+    auto_install = true,
+  },
+}
