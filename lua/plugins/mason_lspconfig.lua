3740d797e39ed42ca48461c7b08efb1a7a688b98
Youngrok Park <yrpark99@gmail.com>
2025-08-14 13:39:31 +0900

    주요 언어 LSP 서버 자동 설치

---
+++ lua/plugins/mason_lspconfig.lua
@@ -0,0 +1,16 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "bashls",
+      "clangd",
+      "dockerls",
+      "gopls",
+      "jdtls",
+      "lua_ls",
+      "pyright",
+      "rust_analyzer",
+      "ts_ls"
+    }
+  }
+}
